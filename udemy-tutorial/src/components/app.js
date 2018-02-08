020e0d755f0ce7aab19f1b90a3a5a7ff2d245a54
SatoruOsawa <osawa@takram.com>
2018-02-08 11:45:28 +0900

    fin: mouseOver and Out

--- udemy-tutorial/src/components/app.js
+++ udemy-tutorial/src/components/app.js
@@ -3,11 +3,27 @@ import React, { Component } from 'react'
 import Greeting from './greeting'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "Bob",
+    }
+  }
+
+  handleMouseOver() {
+    this.setState({ name: 'Bob' })
+  }
+
+  handleMouseOut() {
+    this.setState({ name: 'Mike' })
+  }
+
   render() {
     return (
-      <div>
-        <Greeting name="Bob" />
-        <Greeting name="John" />
+      <div
+        onMouseOver={() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}>
+        <Greeting name={this.state.name} />
       </div>)
   }
 }
