d958cdb2c717c1b255fb2c47b737f606087fd769
SatoruOsawa <osawa@takram.com>
2018-02-18 17:32:02 +0900

    fin: SearchFormコンポーネントの実装

--- udemy-tutorial/src/components/app.js
+++ udemy-tutorial/src/components/app.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react'
 
-import Greeting from './greeting'
+import SearchForm from './SearchForm'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: "Bob",
     }
   }
 
-  handleNameChange(name) {
-    this.setState({ name })
+  handlePlaceSubmit(place) {
+    console.log(place)
   }
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={e => this.handleNameChange(e.target.value)}/>
-        <Greeting name={this.state.name} />
-      </div>)
+        <h1>緯度経度検索</h1>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)}/>
+      </div>
+    )
   }
 }
 
